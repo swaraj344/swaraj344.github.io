83ab89aa38b19022bce430d788f3c39950a0a1a8
Swaraj kumar <swarajk99@gmail.com>
2020-08-29 13:56:35 +0530

    Updates

---
+++ precache-manifest.7b060e765fb0f982f6d04d7a7b0671c3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff5e1b1e1de043f3ee44aebd7ba050d",
+    "url": "/swaraj344.github.io/index.html"
+  },
+  {
+    "revision": "21e9c2987ad83e6022e2",
+    "url": "/swaraj344.github.io/static/css/2.a3c0ce4c.chunk.css"
+  },
+  {
+    "revision": "61896bebe2fbe0b9a971",
+    "url": "/swaraj344.github.io/static/css/main.09c04e9a.chunk.css"
+  },
+  {
+    "revision": "21e9c2987ad83e6022e2",
+    "url": "/swaraj344.github.io/static/js/2.dce9a316.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/swaraj344.github.io/static/js/2.dce9a316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61896bebe2fbe0b9a971",
+    "url": "/swaraj344.github.io/static/js/main.59386960.chunk.js"
+  },
+  {
+    "revision": "991cdccf91ffe9bb9813",
+    "url": "/swaraj344.github.io/static/js/runtime-main.af5d2924.js"
+  },
+  {
+    "revision": "06ab33ae3e2cbae0f509cd05f2ee6793",
+    "url": "/swaraj344.github.io/static/media/boxicons.06ab33ae.eot"
+  },
+  {
+    "revision": "5bbebd73a37de0cc242e872b4483269e",
+    "url": "/swaraj344.github.io/static/media/boxicons.5bbebd73.woff"
+  },
+  {
+    "revision": "5e99edcb80abc73c7a06aacfddca51bf",
+    "url": "/swaraj344.github.io/static/media/boxicons.5e99edcb.svg"
+  },
+  {
+    "revision": "72589e162de78952d4d6b5ce8b832175",
+    "url": "/swaraj344.github.io/static/media/boxicons.72589e16.ttf"
+  },
+  {
+    "revision": "aeaf4ee6e2c712532f982c8a4dd68b16",
+    "url": "/swaraj344.github.io/static/media/boxicons.aeaf4ee6.woff2"
+  },
+  {
+    "revision": "c5ccf11dafa695d4b919570d95cd8a24",
+    "url": "/swaraj344.github.io/static/media/man2.c5ccf11d.jpg"
+  }
+]);
