b2adb92c8df8667f12e18da598808e25a82ec6a8
Swaraj kumar <swarajk99@gmail.com>
2020-10-31 21:50:54 +0530

    half work done

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,156 +1,141 @@
 import React from "react";
-import styled from "styled-components";
-// TODO :https://bootstrapious.com/p/circular-progress-bar
-import { LinearProgress } from "@material-ui/core/";
+import "./css/Resume.css";
 
-const Section = styled.section`
-  color: black;
+const EduDetail = (props) => (
+  <dir
+    className={`edu-detail-card d-flex  ${
+      props.isTop ? "flex-column" : "flex-column-reverse"
+    }`}
+  >
+    <div className={props.position === "left" ? "text-left" : "text-right"}>
+      <h2 className="text-primary">{props.title}</h2>
+      <h5 className="text-dark">{props.subtitle}</h5>
+      <p className="text-secondary">{props.address}</p>
+      <p className="text-info">
+        <span className="text-warning font-italic">Year of Completion: </span>
+        {props.year}
+      </p>
+    </div>
+  </dir>
+);
 
-  padding: 5% 10%;
-  background: #ebeeee;
-`;
-const ResumeLeft = styled.div`
-  width: 25%;
-`;
-
-const Title = styled.h1`
-  font-weight: bold;
-  position: relative;
-  font-size: 28px;
-  &:after {
-    content: "";
-    position: absolute;
-    top: 40px;
-    left: 0;
-
-    /* transform: translate(-130px, 40px); */
-    background: blue;
-    height: 2px;
-    width: 200px;
-  }
-`;
-const Heading = styled.h1`
-  position: relative;
-  font-size: 24px;
-`;
-const Hr = styled.div`
-  margin-top: 30px;
-  margin-bottom: 10px;
-  height: 1px;
-  box-shadow: 0 4px 1px 0 rgba(0, 0, 0, 0.2);
-`;
-const ResumeRight = styled.div`
-  flex: 1;
-  span {
-    color: #afafaf;
-    font-style: italic;
-    font-weight: 400;
-  }
-  p {
-    font-size: 20px;
-    /* font-weight: bold; */
-  }
-`;
-const Card = styled.div`
-  margin: 20px 0;
-  padding: 20px 10px;
-  transition: 0.3s;
-
-  display: flex;
-
-  &:hover {
-    box-shadow: 0px 8px 8px 0px rgba(0, 0, 0, 0.2);
-    transform: translateY(-5px);
-    /* border: 1px solid #afafaf40; */
-  }
-`;
-
-const Spacer = styled.div`
-  height: ${(props) => props.height ?? "30px"};
-`;
 function Resume() {
   return (
     <div>
-      <Section id="resume">
-        <Card>
-          <ResumeLeft>
-            <Title>Education</Title>
-          </ResumeLeft>
-          <ResumeRight>
-            <Heading>Bachelor's Of Computer Application(BCA)|2017-20</Heading>
-            <p>
-              <span>-from:</span> Marwari College Ranchi
-            </p>
-            <p>
-              <span>-year of Graduate:</span> 2017-2020
-            </p>
-            <Hr />
-            <Heading>
-              12<sup>th</sup> Science | JAC Board |2017
-            </Heading>
-            <p>
-              <span>-from:</span> IGSI College,Mandu Ramgarh
-            </p>
-            <p>
-              <span>-year of passing:</span> 2017
-            </p>
-            <Hr />
-            <Heading>
-              10<sup>th</sup> | CBSE board | 2015
-            </Heading>
-            <p>
-              <span>-from:</span> Radha Govind Public School,Ramgarh
-            </p>
-            <p>
-              <span>-year of passing:</span> 2015
-            </p>
-            <Hr />
-          </ResumeRight>
-        </Card>
-
-        <Card>
-          <ResumeLeft>
-            <Title> Skills</Title>
-          </ResumeLeft>
-          <ResumeRight>
-            <Heading>Core Java</Heading>
-            <Spacer />
-            <LinearProgress value={80} variant="buffer" valueBuffer={90} />
-            <Spacer height="10px" />
-            <Heading>Python</Heading>
-            <Spacer />
-            <LinearProgress value={70} variant="buffer" valueBuffer={75} />
-            <Spacer height="10px" />
-            <Heading>JavaScript</Heading>
-            <Spacer />
-            <LinearProgress value={65} variant="buffer" valueBuffer={70} />
-            <Spacer height="10px" />
-            <Heading>ReactJS</Heading>
-            <Spacer />
-            <LinearProgress value={77} variant="buffer" valueBuffer={80} />
-            <Spacer height="10px" />
-            <Heading>NodeJS</Heading>
-            <Spacer />
-            <LinearProgress value={75} variant="buffer" valueBuffer={90} />
-            <Spacer height="10px" />
-            <Heading>Flutter | Dart</Heading>
-            <Spacer />
-            <LinearProgress value={75} variant="buffer" valueBuffer={80} />
-            <Spacer height="10px" />
-            <Heading>Git</Heading>
-            <Spacer />
-            <LinearProgress value={80} variant="buffer" valueBuffer={85} />
-            <Spacer height="10px" />
-            <Heading>HTML | CSS</Heading>
-            <Spacer />
-            <LinearProgress value={90} variant="buffer" valueBuffer={99} />
-            <Spacer height="10px" />
-            <Heading>Bootstrap</Heading>
-            <Spacer />
-            <LinearProgress value={85} variant="buffer" valueBuffer={90} />
-          </ResumeRight>
-        </Card>
-      </Section>
+      <section id="resume">
+        <div className="container-fluid bg-white">
+          <div className="container">
+            <h2 className="text-center title">Resume</h2>
+            <span className="d-flex justify-content-center mt-5">
+              <h4 className="edu-subtitle">EDUCATION</h4>
+            </span>
+            <div className="row">
+              <div className="col-md-6">
+                <EduDetail
+                  isTop={true}
+                  position="right"
+                  title={["10", <sup>th</sup>, " - Matriculation"]}
+                  subtitle="Radha Govind Public School"
+                  year="2015"
+                  address={["Ramgarh Cantt - 829122", <br />, "Jharkhand"]}
+                ></EduDetail>
+              </div>
+              <div className="col-md-6">
+                <EduDetail
+                  position="left"
+                  title={["12", <sup>th</sup>, " - Intermediate of Science"]}
+                  subtitle="Radha Govind Public School"
+                  address={["Ramgarh Cantt - 829122", <br />, "Jharkhand"]}
+                  year="2017"
+                ></EduDetail>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <EduDetail
+                  isTop={true}
+                  position="right"
+                  title={"BCA - Computer Application"}
+                  subtitle="Marwari Collage Ranchi"
+                  address={["Ranchi - 834001", <br />, "Jharkhand"]}
+                  year="2017-2020"
+                ></EduDetail>
+              </div>
+            </div>
+            <span className="d-flex justify-content-center">
+              <h4 className="edu-subtitle">EDUCATION</h4>
+            </span>
+            {/* <div className="container text-dark">
+              <div className="row">
+                <div className="col-lg-3 col-md-4 col-6">
+                  <div className="card">
+                    <div className="box">
+                      <div className="percent">
+                        <svg>
+                          <circle cx="70" cy="70" r="70"></circle>
+                          <circle cx="70" cy="70" r="70"></circle>
+                        </svg>
+                        <div className="number">
+                          <h2>
+                            90 <span>%</span>
+                          </h2>
+                        </div>
+                      </div>
+                      <h2 className="skill-title">html</h2>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-3 col-md-4 col-6">
+                  <div className="card">
+                    <div className="box">
+                      <div className="percent">
+                        <svg>
+                          <circle cx="70" cy="70" r="70"></circle>
+                          <circle cx="70" cy="70" r="70"></circle>
+                        </svg>
+                        <div className="number">
+                          <h2>
+                            90 <span>%</span>
+                          </h2>
+                        </div>
+                      </div>
+                      <h2 className="skill-title">html</h2>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-3 col-md-4 col-6">
+                  <div className="card">
+                    <div className="box">
+                      <div className="">
+                        <svg>
+                          <circle cx="70" cy="70" r="70"></circle>
+                          <circle cx="70" cy="70" r="70"></circle>
+                        </svg>
+                        <div className="number">
+                          <h2>
+                            90 <span>%</span>
+                          </h2>
+                        </div>
+                      </div>
+                      <h2 className="skill-title">html</h2>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-3 col-md-4 col-6">
+                  <div className="circularprogress">
+                    <div className="svg-container bg-danger">
+                      <svg>
+                        <circle cx="70" cy="70" r="70"></circle>
+                        <circle cx="70" cy="70" r="70"></circle>
+                      </svg>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div> */}
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
